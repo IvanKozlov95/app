73b32828f5c6e468234b9d8eed2a753ab6a01248
IvanKozlov95 <ikozlov9000@gmail.com>
2016-05-28 22:13:58 +0300

    Dynamic company search

---
+++ public/js/company/search.js
@@ -0,0 +1,37 @@
+$(window).load(() => {
+	$('#searchlist').btsListFilter('#searchinput', {
+	sourceTmpl: '<div class="company-div list-group-item">\
+					<div class="row">\
+						<div class="col-sm-4">\
+							<h3 list-group-item-heading" style="display:inline-block;">\
+								{name}\
+							</h3>\
+							<a href="/company/?id={_id}"> \
+								<span class="btn glyphicon glyphicon-info-sign list-group-item">\
+								</span>\
+							</a>\
+							<a id="modal-link"> \
+								<span class="btn glyphicon glyphicon glyphicon-edit list-group-item" data-toggle="modal" data-target="#exampleModal" data-companyid={_id}>\
+								</span>\
+							</a>\
+						</div>\
+					</div>\
+					<div class="row">\
+						<div class="col-sm-10">\
+							<p class="list-group-item-text">\
+								{description}\
+							</p>\
+						</div>\
+						<div calss="col-sm-2 col-sm-offset-10" >\
+							<img src="/avatars/{avatar}" height="100" width="100" />\
+						</div>\
+					</div>\
+				</div>',
+		sourceData: function(text, callback) {
+			return $.getJSON('search/?name='+text, function(json) {
+				callback(json);
+			});
+		},
+		resetOnBlur: false
+	});
+})
